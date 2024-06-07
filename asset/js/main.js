7cc17bf3047719b3e277efd6f220747c11ed1986
ubayirvian <bay.irvian@gmail.com>
2024-06-07 17:50:42 +0700

    update repo

--- asset/js/main.js
+++ asset/js/main.js
@@ -38,3 +38,35 @@ const sr = ScrollReveal({
 sr.reveal(`.home__data`, {origin: 'top', delay: 400})
 sr.reveal(`.home__img`, {origin: 'bottom', delay: 600})
 sr.reveal(`.home__footer`, {origin: 'bottom', delay: 800})
+
+
+/*======================== SLIDER SERVICES PAGE =======================*/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
